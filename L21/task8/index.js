d9f67343110e80ac5bcb324839fde7f3b60c4cc1
Bohdan <drabynko.b@gmail.com>
2022-07-07 22:20:47 +0300

    done

---
+++ L21/task8/index.js
@@ -0,0 +1,7 @@
+export function createButton(buttonText) {
+  const button = document.createElement('button');
+  button.textContent = buttonText;
+  document.querySelector('body').append(button);
+}
+
+createButton('asdh;gljahsdgkhj');
