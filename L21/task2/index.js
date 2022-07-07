c561682073023182173230916476722ae4c411b5
Bohdan <drabynko.b@gmail.com>
2022-07-07 20:12:12 +0300

    done

---
+++ L21/task2/index.js
@@ -0,0 +1,13 @@
+export function getTitleElement() {
+  const titleElem = document.querySelector('.title');
+  console.dir(titleElem);
+}
+
+getTitleElement();
+
+export function getInputElement() {
+  const inputElem = document.querySelector('input[type=text]');
+  console.dir(inputElem);
+}
+
+getInputElement();
