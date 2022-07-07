80443e90405dde8da9124ef2202d5a1dbd1c7e0f
Bohdan <drabynko.b@gmail.com>
2022-07-08 01:20:27 +0300

    done?

--- L21/task12/index.js
+++ L21/task12/index.js
@@ -1,10 +1,16 @@
+// export function squaredNumbers() {
+//   const data1 = document.querySelector('[data-number="5"]');
+//   data1.dataset.squaredNumber = data1.dataset.number * data1.dataset.number;
+
+//   const data2 = document.querySelector('[data-number="7"]');
+//   data2.dataset.squaredNumber = data2.dataset.number * data2.dataset.number;
+
+//   const data3 = document.querySelector('[data-number="-3"]');
+//   data3.dataset.squaredNumber = data3.dataset.number * data3.dataset.number;
+// }
+
 export function squaredNumbers() {
-  const data1 = document.querySelector('[data-number="5"]');
-  data1.dataset.squaredNumber = data1.dataset.number * data1.dataset.number;
-
-  const data2 = document.querySelector('[data-number="7"]');
-  data2.dataset.squaredNumber = data2.dataset.number * data2.dataset.number;
-
-  const data3 = document.querySelector('[data-number="-3"]');
-  data3.dataset.squaredNumber = data3.dataset.number * data3.dataset.number;
+  Array.from(document.querySelectorAll('.number')).forEach(
+    el => (el.dataset.squaredNumber = el.dataset.number * el.dataset.number),
+  );
 }
