efcb9fa286cfd6d00061a59440bdb76b39a062a2
Bohdan <drabynko.b@gmail.com>
2022-07-08 01:06:40 +0300

    fix

--- L21/task12/index.js
+++ L21/task12/index.js
@@ -1,10 +1,11 @@
 export function squaredNumbers() {
   const data1 = document.querySelector('[data-number="5"]');
-  data1.setAttribute('data-squared-number', data1.dataset.number * data1.dataset.number);
+  data1.dataset.squaredNumbers = data1.dataset.number * data1.dataset.number;
 
   const data2 = document.querySelector('[data-number="7"]');
-  data2.setAttribute('data-squared-number', data2.dataset.number * data2.dataset.number);
+  data2.dataset.squaredNumbers = data2.dataset.number * data2.dataset.number;
 
   const data3 = document.querySelector('[data-number="-3"]');
-  data3.setAttribute('data-squared-number', data3.dataset.number * data3.dataset.number);
+  data3.dataset.squaredNumbers = data3.dataset.number * data3.dataset.number;
 }
+squaredNumbers();
