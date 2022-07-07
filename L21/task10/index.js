a0712ce44350eb0b97173d2c3ed08b4b30fa06e5
Bohdan <drabynko.b@gmail.com>
2022-07-08 00:12:03 +0300

    fix

--- L21/task10/index.js
+++ L21/task10/index.js
@@ -1,6 +1,7 @@
 export function finishForm() {
   const inputElement = document.createElement('input');
   inputElement.setAttribute('name', 'login');
+  inputElement.setAttribute('type', 'text');
   document.querySelector('.login-form').prepend(inputElement);
   document.querySelector('[name = password]').setAttribute('type', 'password');
 }
