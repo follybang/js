8ba4b9798e0026ac2d5c4d83c8cc38d5fe04a84c
Bohdan <drabynko.b@gmail.com>
2022-07-08 15:42:39 +0300

    done

--- L22/task1/index.js
+++ L22/task1/index.js
@@ -1,1 +1,49 @@
-function huasd() {}
+const divElem = document.querySelector('.rect_div');
+const pElem = document.querySelector('.rect_p');
+const spanElem = document.querySelector('.rect_span');
+const clearButton = document.querySelector('.clear-btn');
+const attachButton = document.querySelector('.attach-handlers-btn');
+const removeButton = document.querySelector('.remove-handlers-btn');
+const eventsListElem = document.querySelector('.events-list');
+
+const logTarget = (text, color) => {
+  eventsListElem.innerHTML += `<span style="color: ${color}; margn-left: 8px">${text}</span>`;
+};
+
+const logGreenDiv = logTarget.bind(null, 'DIV', 'green');
+const logGreenP = logTarget.bind(null, 'P', 'green');
+const logGreenSpan = logTarget.bind(null, 'SPAN', 'green');
+
+const logGreyDiv = logTarget.bind(null, 'DIV', 'grey');
+const logGreyP = logTarget.bind(null, 'P', 'grey');
+const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
+
+const clearList = () => {
+  eventsListElem.innerHTML = '';
+};
+
+const removeListener = () => {
+  divElem.removeEventListener('click', logGreenDiv);
+  pElem.removeEventListener('click', logGreenP);
+  spanElem.removeEventListener('click', logGreenSpan);
+
+  divElem.removeEventListener('click', logGreyDiv, true);
+  pElem.removeEventListener('click', logGreyP, true);
+  spanElem.removeEventListener('click', logGreySpan, true);
+};
+
+const addListener = () => {
+  divElem.addEventListener('click', logGreenDiv);
+  pElem.addEventListener('click', logGreenP);
+  spanElem.addEventListener('click', logGreenSpan);
+
+  divElem.addEventListener('click', logGreyDiv, true);
+  pElem.addEventListener('click', logGreyP, true);
+  spanElem.addEventListener('click', logGreySpan, true);
+};
+
+addListener();
+
+clearButton.addEventListener('click', clearList);
+removeButton.addEventListener('click', removeListener);
+attachButton.addEventListener('click', addListener);
