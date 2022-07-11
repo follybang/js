781757de68aa322989924c6ed2b8f7de1d54a5b7
Bohdan <drabynko.b@gmail.com>
2022-07-11 17:31:00 +0300

    done

--- L24/task2/index.js
+++ L24/task2/index.js
@@ -4,6 +4,6 @@ const formatter = new Intl.DateTimeFormat('en', {
   hour12: false,
 });
 
-const getTime = date => formatter.format(date);
+const getGreenwichTime = date => formatter.format(date);
 
-console.log(getTime(new Date()));
+console.log(getGreenwichTime(new Date()));
