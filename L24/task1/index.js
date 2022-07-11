08a8d9684b57135ade47c0880a8c9d4a2fe48d4f
Bohdan <drabynko.b@gmail.com>
2022-07-11 17:06:21 +0300

    done

--- L24/task1/index.js
+++ L24/task1/index.js
@@ -1,6 +1,6 @@
 const week = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
 
-function dayOfWeek(date, days) {
+export function dayOfWeek(date, days) {
   const day = new Date(date).getDate();
   const dateInFuturt = new Date(date).setDate(day + days);
   return week[new Date(dateInFuturt).getDay()];
