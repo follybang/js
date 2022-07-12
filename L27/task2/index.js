2ae28c7a3c993171618addd00e669d3237dad200
Bohdan <drabynko.b@gmail.com>
2022-07-12 17:25:35 +0300

    done

---
+++ L27/task2/index.js
@@ -0,0 +1,26 @@
+const counterElem = document.querySelector('.counter');
+const counterValueElem = document.querySelector('.counter__value');
+
+const onCounterChange = e => {
+  const isButton = e.target.classList.contains('counter__button');
+  if (!isButton) {
+    return;
+  }
+
+  const action = e.target.dataset.action;
+  const oldValue = +counterValueElem.textContent;
+  const newValue = action === 'decrease' ? oldValue - 1 : oldValue + 1;
+
+  localStorage.setItem('counterValue', newValue);
+
+  counterValueElem.textContent = newValue;
+};
+
+counterElem.addEventListener('click', onCounterChange);
+
+const onStorageChange = e => {
+  console.log(e);
+  counterValueElem.textContent = e.newValue;
+};
+
+window.addEventListener('storage', onStorageChange);
