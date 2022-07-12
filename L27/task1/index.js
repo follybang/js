443c6ee508c6d31e5e892414e76aa05bf9ffc8de
Bohdan <drabynko.b@gmail.com>
2022-07-12 16:31:26 +0300

    done

---
+++ L27/task1/index.js
@@ -0,0 +1,23 @@
+localStorage.clear();
+localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }));
+localStorage.setItem('name', JSON.stringify('Tom'));
+localStorage.setItem('age', JSON.stringify(17));
+
+const getLocalStorageData = () => {
+  return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch {
+      newValue = value;
+    }
+
+    return {
+      ...acc,
+      [key]: newValue,
+    };
+  });
+};
+
+// console.log(getLocalStorageData());
+// console.log(Object.entries(localStorage));
