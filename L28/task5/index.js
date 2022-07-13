751c0b3774d9fdb2ff0605ef6f12d5aa47a854c4
Bohdan <drabynko.b@gmail.com>
2022-07-13 20:19:06 +0300

    fix

--- L28/task5/index.js
+++ L28/task5/index.js
@@ -1,43 +1,48 @@
-const checker = value => {
-  let modifier = 1;
-  if (value === 'years') {
-    modifier *= 1000 * 60 * 60 * 24 * 30 * 12;
-  } else if (value === 'months') {
-    modifier *= 1000 * 60 * 60 * 24 * 30;
-  } else if (value === 'days') {
-    modifier *= 1000 * 60 * 60 * 24;
-  } else if (value === 'hours') {
-    modifier *= 1000 * 60 * 60;
-  } else if (value === 'minutes') {
-    modifier *= 1000 * 60;
-  } else if (value === 'seconds') {
-    modifier *= 1000;
-  }
-  return modifier;
+const getMethodsNames = {
+  years: 'getFullYear',
+  months: 'getMonth',
+  days: 'getDate',
+  hours: 'getHours',
+  minutes: 'getMinutes',
+  seconds: 'getSeconds',
+  milliseconds: 'getMilliseconds',
 };
 
+const setMethodsNames = {
+  years: 'setFullYear',
+  months: 'setMonth',
+  days: 'setDate',
+  hours: 'setHours',
+  minutes: 'setMinutes',
+  seconds: 'setSeconds',
+  milliseconds: 'setMilliseconds',
+};
 export const shmoment = initualDate => {
   let modifiedDate = new Date(initualDate);
 
   const calc = {
-    add(measurementValue, value) {
-      modifiedDate += value * checker(measurementValue);
+    add(measureValue, value) {
+      const currentMeasureValue = modifiedDate[getMethodsNames[measureValue]]();
+      modifiedDate = new Date(
+        modifiedDate[setMethodsNames[measureValue]](
+          currentMeasureValue + value,
+        ),
+      );
       return this;
     },
-    subtract(measurementValue, value) {
-      modifiedDate -= value * checker(measurementValue);
-      return this;
+    subtract(measureValue, value) {
+      return this.add(measureValue, -value);
     },
     result() {
-      return new Date(modifiedDate);
+      return modifiedDate;
     },
   };
 
   return calc;
 };
 
-// const result = shmoment(new Date(10, 10, 10))
-//   .subtract('years', 200)
-//   .add('months', 5)
+// const result = shmoment(new Date())
+//   .add('years', 1000)
+//   .subtract('months', 9)
 //   .result();
 // console.log(result);
