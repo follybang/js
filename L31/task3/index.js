cf80c7b4c16157516dcfa108cc498b35cce3b6c6
Bohdan <drabynko.b@gmail.com>
2022-07-15 13:39:10 +0300

    done

---
+++ L31/task3/index.js
@@ -0,0 +1,33 @@
+const successPromise = new Promise(resolve => {
+  resolve(32);
+});
+
+/*
+ * исправь цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+ */
+
+successPromise
+  .then(number => {
+    const halfNumber = number / 2;
+    return halfNumber;
+  })
+  .then(number => {
+    const squaredNumber = number * number;
+    return squaredNumber;
+  })
+  .then(result => {
+    console.log(result); // 256
+  });
+
+/*
+ * исправь цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+ */
+successPromise
+  .then(number => number * 10)
+  .then(result => {
+    console.log(result); // 320
+  });
+
+console.log(
+  '!!! Обрати внимание, что этот текст вывелся самым первым. Ведь это синхронный код, а промисы - асинхронны !!!',
+);
---
+++ L31/task5/index.js
@@ -0,0 +1,15 @@
+// input: time
+// output: delayed function execution
+
+// 1. create new promise
+// 2. set delay for resolve
+// 3. return delayed promise
+
+function delay(time) {
+  const pr = new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
+  return pr;
+}
