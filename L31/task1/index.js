8d31d97de6636dce4a23fb8a5b43be5e24b69835
Bohdan <drabynko.b@gmail.com>
2022-07-15 12:51:42 +0300

    done

---
+++ L31/task1/index.js
@@ -0,0 +1,19 @@
+const requestUserData = userId => {
+  const request = new Promise((resolve, reject) => {
+    if (userId === 'broken') {
+      setTimeout(() => {
+        reject(new Error('User not found'));
+      }, 500);
+    } else {
+      setTimeout(() => {
+        resolve({
+          name: 'John',
+          age: 17,
+          userId,
+          email: `${userId}@example.com`,
+        });
+      }, 1000);
+    }
+  });
+  return request;
+};
