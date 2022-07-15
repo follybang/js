9d2182bfc5a8047b448b33a37601b6cdc5d6d0a5
Bohdan <drabynko.b@gmail.com>
2022-07-15 15:45:37 +0300

    done

--- L32/task3/index.js
+++ L32/task3/index.js
@@ -1,8 +1,12 @@
-const makePromise = num =>
-  new Promise(resolve => {
+export const makePromise = num => {
+  const pr = new Promise(resolve => {
     resolve(num);
   });
+  return pr;
+};
 
-// makePromise(17).then(number => {
+// console.log(makePromise(17));
+
+// makePromise(19).then(number => {
 //   console.log(number);
 // });
