f11e814300ad1b3f4c0db3f798897985de50a6db
Bohdan <drabynko.b@gmail.com>
2022-07-26 17:33:17 +0300

    done

---
+++ L35/task2/index.js
@@ -0,0 +1,42 @@
+// const successRequest = Promise.resolve({ name: 'Tom' });
+
+// successRequest
+//   .then(data => {
+//     console.log(data);
+//     throw new Error('Unexpected error');
+//   })
+//   .catch(err => {
+//     console.warn(err);
+//   });
+
+// const failedRequest = Promise.reject(new Error('Fail'));
+
+// failedRequest
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.warn(err.message);
+//     throw err;
+//   })
+//   .then(data => {
+//     console.log(data);
+//   });
+
+// window.addEventListener('unhandledrejection', function (e) {
+//   console.log(e.reason.message);
+// });
+
+fetch('https://api.github.com/user/github')
+  .then(response => {
+    debugger;
+    if (response.status === 200) {
+      return response.json();
+    }
+    throw new Error('No data');
+  })
+  .then(data => console.log(data))
+  .catch(err => {
+    debugger;
+    console.log(err);
+  });
